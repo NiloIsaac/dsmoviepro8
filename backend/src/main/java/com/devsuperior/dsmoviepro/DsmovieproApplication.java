c849dcf0435ffda5894e9e6033f5d00720b07bb6
Nilo Isaac <niloisaac2@gmail.com>
2022-05-12 13:59:31 -0300

    project created

---
+++ backend/src/main/java/com/devsuperior/dsmoviepro/DsmovieproApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmoviepro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmovieproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsmovieproApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dsmoviepro/DsmovieproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmoviepro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmovieproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
